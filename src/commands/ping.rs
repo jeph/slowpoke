e08ebb453c80f4df5a077276aa9b69539d3aceea
Jeph Liu <jephliu@jeph.io>
2025-03-23 21:49:53 -0400

    feat: implement basic ping bot

---
+++ src/commands/ping.rs
@@ -0,0 +1,17 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, crate::Data, crate::Error>;
+
+#[poise::command(slash_command, prefix_command)]
+pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
+  let now = SystemTime::now();
+  let current_epoch_ms = now.duration_since(UNIX_EPOCH).unwrap().as_millis() as i64;
+
+  let msg_timestamp_ms = ctx.created_at().timestamp_millis();
+
+  let elapsed_time_ms = current_epoch_ms - msg_timestamp_ms;
+  let response = format!("pong! latency: {} ms", elapsed_time_ms);
+  ctx.say(response).await?;
+  Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+mod commands;
+use crate::commands::ping::ping;
+use dotenvy::dotenv;
+use poise::serenity_prelude::{
+  ActivityData, ActivityType, ClientBuilder, GatewayIntents,
+};
+use poise::FrameworkOptions;
+use std::env;
+
+struct Data {}
+type Error = Box<dyn std::error::Error + Send + Sync>;
+
+#[tokio::main]
+async fn main() {
+  dotenv().ok();
+
+  let token =
+    env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN was not found in the environment");
+
+  let framework = poise::Framework::builder()
+    .options(FrameworkOptions {
+      commands: vec![ping()],
+      ..Default::default()
+    })
+    .setup(|context, _ready, framework| {
+      Box::pin(async move {
+        poise::builtins::register_globally(context, &framework.options().commands)
+          .await?;
+
+        let activity_data = ActivityData {
+          name: "Pokemon".to_string(),
+          kind: ActivityType::Playing,
+          state: None,
+          url: None,
+        };
+        context.set_activity(Some(activity_data));
+        Ok(Data {})
+      })
+    })
+    .build();
+
+  let intents = GatewayIntents::non_privileged();
+
+  let client = ClientBuilder::new(token, intents)
+    .framework(framework)
+    .await;
+  client.unwrap().start().await.unwrap();
+}
