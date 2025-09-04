694c30c924ecdca60c7803b6cf7f459e6ff2fa1d
jeph <23463082+jeph@users.noreply.github.com>
2025-09-04 18:13:16 -0400

    fix eslint with roll command

--- src/commands/roll.ts
+++ src/commands/roll.ts
@@ -1,5 +1,4 @@
-
-import { SlashCommandBuilder, ChatInputCommandInteraction, AttachmentBuilder, EmbedBuilder } from 'discord.js'
+import { ChatInputCommandInteraction, EmbedBuilder, SlashCommandBuilder } from 'discord.js'
 import { SlashCommand } from '../models/commands'
 
 const COLORS = [
@@ -7,33 +6,33 @@ const COLORS = [
   0xF9E2AF, 0xA6E3A1, 0x94E2D5, 0x89DCEB, 0x74C7EC, 0x89B4FA, 0xB4BEFE,
 ]
 
-function getRandomColor() {
-    return COLORS[Math.floor(Math.random() * COLORS.length)]
+function getRandomColor () {
+  return COLORS[Math.floor(Math.random() * COLORS.length)]
 }
 
 export const createRollCommand = (): SlashCommand => ({
-    command: new SlashCommandBuilder()
-      .setName('roll')
-      .setDescription('Roll a dice with custom sides')
-      .addIntegerOption(option =>
-        option
-          .setName('sides')
-          .setDescription('Number of sides on the dice (default is 6)')
-          .setRequired(false)
-          .setMinValue(2)
-          .setMaxValue(100)
-      ),
-    async execute(interaction: ChatInputCommandInteraction) {
-        const sides = interaction.options.getInteger('sides') || 6
-        if (sides < 2 || sides > 100) {
-            await interaction.reply({ content: 'Please provide a valid number of sides between 2 and 100.', ephemeral: true })
-            return
-        }
-        const diceRoll = Math.floor(Math.random() * sides) + 1
-        const embed = new EmbedBuilder()
-            .setColor(getRandomColor()) 
-            .setTitle('Dice Roll')
-            .setDescription(`🎲 You rolled a **${diceRoll}** on a D${sides}!`)
-        await interaction.reply({ embeds: [embed] })
+  command: new SlashCommandBuilder()
+    .setName('roll')
+    .setDescription('Roll a dice with custom sides')
+    .addIntegerOption(option =>
+      option
+        .setName('sides')
+        .setDescription('Number of sides on the dice (default is 6)')
+        .setRequired(false)
+        .setMinValue(2)
+        .setMaxValue(100)
+    ),
+  async execute (interaction: ChatInputCommandInteraction) {
+    const sides = interaction.options.getInteger('sides') || 6
+    if (sides < 2 || sides > 100) {
+      await interaction.reply({ content: 'Please provide a valid number of sides between 2 and 100.', ephemeral: true })
+      return
     }
-  })
+    const diceRoll = Math.floor(Math.random() * sides) + 1
+    const embed = new EmbedBuilder()
+      .setColor(getRandomColor())
+      .setTitle('Dice Roll')
+      .setDescription(`🎲 You rolled a **${diceRoll}** on a D${sides}!`)
+    await interaction.reply({ embeds: [embed] })
+  }
+})
