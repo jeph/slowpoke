abca2817e533bda2693dcf2c7ccfd6234481c210
jeph <23463082+jeph@users.noreply.github.com>
2025-09-04 02:07:09 -0400

    handle nit

--- src/main.ts
+++ src/main.ts
@@ -99,9 +99,9 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
     if (interaction.replied || interaction.deferred) {
       await interaction.followUp({ content: errorMessage, ephemeral: true })
-    } else {
-      await interaction.reply({ content: errorMessage, ephemeral: true })
+      return
     }
+    await interaction.reply({ content: errorMessage, ephemeral: true })
   }
 })
 
